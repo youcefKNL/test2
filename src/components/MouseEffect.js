814a159cda5208e2d7b2cbd8e4b382a5894077ac
youcefKNL <khenichil_youcef@hotmail.fr>
2023-09-05 20:21:46 +0200

    fin legal logo

--- src/components/MouseEffect.js
+++ src/components/MouseEffect.js
@@ -1,36 +1,55 @@
-import Logo from "../assets/logo-Legalis2-removebg-preview.png";
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+
+// function MouseEffect() {
+//   useEffect(() => {
+//     const mouses = document.querySelectorAll(".mouse");
+
+//     const handleMouseMove = (e) => {
+//       mouses.forEach((mouse) => {
+//         mouse.style.top = e.clientY + "px";
+//         mouse.style.left = e.clientX + "px";
+
+//       });
+//     };
+
+//     window.addEventListener("mousemove", handleMouseMove);
+
+//     return () => {
+//       // Nettoyez l'écouteur d'événements lorsque le composant est démonté
+//       window.removeEventListener("mousemove", handleMouseMove);
+//     };
+//   }, []); // Le tableau vide signifie que cela s'exécute une seule fois à l'initialisation
+
+//   return <div className="mouse"></div>;
+// }
+
+// export default MouseEffect;
+import React, { useEffect, useState } from "react";
 
 function MouseEffect() {
-  useEffect(() => {
-    const mouses = document.querySelectorAll(".mouse");
+  const [mousePosition, setMousePosition] = useState({ top: 0, left: 0 });
 
+  useEffect(() => {
     const handleMouseMove = (e) => {
-      mouses.forEach((mouse) => {
-        mouse.style.top = e.clientY + "px";
-        mouse.style.left = e.clientX + "px";
+      setMousePosition({
+        top: e.clientY + window.scrollY,
+        left: e.clientX + window.scrollX,
       });
     };
 
     window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      // Nettoyez l'écouteur d'événements lorsque le composant est démonté
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []); // Le tableau vide signifie que cela s'exécute une seule fois à l'initialisation
+  }, []);
 
-  const mouseStyle = {
-    position: "fixed",
-    width: "30px", // Vous pouvez ajuster la largeur selon vos besoins
-    height: "30px", // Vous pouvez ajuster la hauteur selon vos besoins
-    borderRadius: "50%",
-    transform: "translate(20%, 20%)",
-    background: `url(${Logo}) center/cover`, // Correction ici : utilisez (${Logo}) pour inclure la variable Logo
-    transition: "d0.3s",
-  };
-
-  return <div className="mouse" style={mouseStyle}></div>;
+  return (
+    <div
+      className="mouse"
+      style={{ top: mousePosition.top + "px", left: mousePosition.left + "px" }}
+    ></div>
+  );
 }
 
 export default MouseEffect;
